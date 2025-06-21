60a1b2f5b758963869e59c582fdcfce619198b87
YangNu <bin450579@gmail.com>
2025-06-21 22:28:17 +0900

    [feat]: 지도에 분리수거장 위치 핀 추가

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,15 +1,33 @@
 import * as Location from "expo-location";
-import React, { useEffect } from "react";
-import { NaverMapView, Region } from "@mj-studio/react-native-naver-map";
+import React, { useEffect, useState } from "react";
+import {
+  NaverMapMarkerOverlay,
+  NaverMapView,
+  Region,
+} from "@mj-studio/react-native-naver-map";
 import { useLocationPermission } from "react-native-vision-camera";
 
+import locations from "@/lib/location.json";
+import { VStack } from "@/components/ui/vstack";
+import { HStack } from "@/components/ui/hstack";
+import { TouchableOpacity, Text } from "react-native";
+import { ButtonText, Button } from "@/components/ui/button";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { CloseIcon, Icon } from "@/components/ui/icon";
+
+type LocationInfo = {
+  address: string;
+  time: string;
+  department: string;
+  phone: string;
+  latitude: number;
+  longitude: number;
+};
+
 export default function Page() {
-  const jejuRegion: Region = {
-    latitude: 33.20530773,
-    longitude: 126.14656715029,
-    latitudeDelta: 0.38,
-    longitudeDelta: 0.8,
-  };
+  const inset = useSafeAreaInsets();
+
+  const [index, setIndex] = useState<number>(-1);
 
   useEffect(() => {
     (async () => {
@@ -29,12 +47,62 @@ export default function Page() {
   }, []);
 
   return (
-    <NaverMapView
-      style={{ flex: 1 }}
-      locale="ko"
-      logoAlign={"BottomRight"}
-      onInitialized={() => console.log("initialized!")}
-      onOptionChanged={() => console.log("Option Changed!")}
-    ></NaverMapView>
+    <>
+      <NaverMapView
+        style={{ flex: 1 }}
+        locale="ko"
+        logoAlign={"BottomRight"}
+        onInitialized={() => console.log("initialized!")}
+        onOptionChanged={() => console.log("Option Changed!")}
+      >
+        {locations.map((e, i) => {
+          return (
+            <>
+              <NaverMapMarkerOverlay
+                latitude={e.latitude}
+                longitude={e.longitude}
+                onTap={() => {
+                  setIndex(i);
+                }}
+                tintColor={index==i ? "red" : undefined}
+              />
+            </>
+          );
+        })}
+      </NaverMapView>
+
+      {index != -1 && (
+        <VStack
+          className={`absolute bg-white p-5 left-5 right-5 rounded-xl gap-2`}
+          style={{ bottom: inset.bottom }}
+        >
+          <HStack className="items-center justify-between">
+            <Text className="w-[80%]">{locations[index].address}</Text>
+            {/* <Image source={{}}></Image> */}
+            <TouchableOpacity className="p-2" onPress={() => {
+              setIndex(-1);
+            }}>
+              <Icon as={CloseIcon} size="xl" />
+            </TouchableOpacity>
+          </HStack>
+          <HStack>
+            <Text className="w-[25%]">운영시간</Text>
+            <Text>{locations[index].time}</Text>
+          </HStack>
+          <HStack>
+            <Text className="w-[25%]">담당부서</Text>
+            <Text>{locations[index].department}</Text>
+          </HStack>
+          <HStack>
+            <Text className="w-[25%]">전화번호</Text>
+            <Text>{locations[index].phone}</Text>
+          </HStack>
+          
+          <HStack className="gap-4">
+            
+          </HStack>
+        </VStack>
+      )}
+    </>
   );
 }
