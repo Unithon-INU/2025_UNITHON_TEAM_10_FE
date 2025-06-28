f881ef009d23bccd0aa1c78474506d4d1fa9f64a
YangNu <bin450579@gmail.com>
2025-06-28 11:17:50 +0900

    [feat]: 지도 핀 기능 롤백

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,86 +1,105 @@
-import React, { useRef, useState } from "react";
-import { Text, TouchableOpacity } from "react-native";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
 import {
   NaverMapMarkerOverlay,
   NaverMapView,
-  NaverMapViewRef,
   Region,
 } from "@mj-studio/react-native-naver-map";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useLocationPermission } from "react-native-vision-camera";
 
+import locations from "@/lib/location.json";
 import { VStack } from "@/components/ui/vstack";
 import { HStack } from "@/components/ui/hstack";
-import { Icon, CloseIcon } from "@/components/ui/icon";
-import useDebounce from "@/lib/useDebounce";
-import RecycleCenterApi, { LocationInfo } from "@/api/recycleCenter";
-import useUserLocation from "@/lib/useUserLocation";
+import { TouchableOpacity, Text } from "react-native";
+import { ButtonText, Button } from "@/components/ui/button";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { CloseIcon, Icon } from "@/components/ui/icon";
+
+type LocationInfo = {
+  address: string;
+  time: string;
+  department: string;
+  phone: string;
+  latitude: number;
+  longitude: number;
+};
 
 export default function Page() {
-  const insets = useSafeAreaInsets();
-  const mapRef = useRef<NaverMapViewRef>(null);
+  const inset = useSafeAreaInsets();
 
-  const { initialRegion } = useUserLocation();
-  const [index, setIndex] = useState(-1);
-  const [recycleLocations, setRecycleLocations] = useState<LocationInfo[]>([]);
+  const [index, setIndex] = useState<number>(-1);
 
-  const fetchLocations = useDebounce(async (region: Region) => {
-    const radiusKm = Math.min(
-      (region.latitudeDelta / 2) * 111,
-      (region.longitudeDelta / 2) *
-        111 *
-        Math.cos(region.latitude * (Math.PI / 180))
-    );
-    const centers = await RecycleCenterApi.fetchCenters(
-      region.latitude,
-      region.longitude,
-      radiusKm
-    );
-    if (centers)
-      setRecycleLocations((prev) => Array.from(new Set([...prev, ...centers])));
-  }, 1000);
-  if (!initialRegion) return <></>;
+  useEffect(() => {
+    (async () => {
+      try {
+        const { granted } = await Location.requestForegroundPermissionsAsync();
+        /**
+         * Note: Foreground permissions should be granted before asking for the background permissions
+         * (your app can't obtain background permission without foreground permission).
+         */
+        if (granted) {
+          await Location.requestBackgroundPermissionsAsync();
+        }
+      } catch (e) {
+        console.error(`Location request has been failed: ${e}`);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <NaverMapView
-        ref={mapRef}
         style={{ flex: 1 }}
         locale="ko"
-        logoAlign="BottomRight"
-        initialCamera={{ ...initialRegion, zoom: 13 }}
-        onCameraChanged={(e) => fetchLocations(e.region)}
-        onInitialized={() => console.log("Map Initialized")}
+        logoAlign={"BottomRight"}
+        onInitialized={() => console.log("initialized!")}
+        onOptionChanged={() => console.log("Option Changed!")}
       >
-        {recycleLocations.map((e, i) => (
-          <NaverMapMarkerOverlay
-            key={`${e.latitude}-${e.longitude}-${i}-${e.name}`}
-            latitude={e.latitude}
-            longitude={e.longitude}
-            onTap={() => setIndex(i)}
-            tintColor={index === i ? "red" : undefined}
-          />
-        ))}
+        {locations.map((e, i) => {
+          return (
+            <>
+              <NaverMapMarkerOverlay
+                latitude={e.latitude}
+                longitude={e.longitude}
+                onTap={() => {
+                  setIndex(i);
+                }}
+                tintColor={index==i ? "red" : undefined}
+              />
+            </>
+          );
+        })}
       </NaverMapView>
 
-      {index !== -1 && (
+      {index != -1 && (
         <VStack
-          className="absolute bg-white p-5 left-5 right-5 rounded-xl gap-2"
-          style={{ bottom: insets.bottom }}
+          className={`absolute bg-white p-5 left-5 right-5 rounded-xl gap-2`}
+          style={{ bottom: inset.bottom }}
         >
           <HStack className="items-center justify-between">
-            <Text>{recycleLocations[index].name}</Text>
-            <TouchableOpacity className="p-2" onPress={() => setIndex(-1)}>
+            <Text className="w-[80%]">{locations[index].address}</Text>
+            {/* <Image source={{}}></Image> */}
+            <TouchableOpacity className="p-2" onPress={() => {
+              setIndex(-1);
+            }}>
               <Icon as={CloseIcon} size="xl" />
             </TouchableOpacity>
           </HStack>
           <HStack>
-            <Text style={{ flex: 1 }}>주소</Text>
-            <Text style={{ flex: 9 }}>{recycleLocations[index].address}</Text>
+            <Text className="w-[25%]">운영시간</Text>
+            <Text>{locations[index].time}</Text>
           </HStack>
           <HStack>
-            <Text style={{ flex: 1 }}>유형</Text>
-            <Text style={{ flex: 9 }}>
-              {recycleLocations[index].specialWasteType}
-            </Text>
+            <Text className="w-[25%]">담당부서</Text>
+            <Text>{locations[index].department}</Text>
+          </HStack>
+          <HStack>
+            <Text className="w-[25%]">전화번호</Text>
+            <Text>{locations[index].phone}</Text>
+          </HStack>
+          
+          <HStack className="gap-4">
+            
           </HStack>
         </VStack>
       )}
